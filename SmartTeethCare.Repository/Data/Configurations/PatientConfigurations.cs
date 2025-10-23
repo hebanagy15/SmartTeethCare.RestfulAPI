0740d89455484c89b5afcca07bbb7d2b6cd38a3f
Heba Nagy <hebanagy3333@gmail.com>
2025-10-23 20:08:48 +0300

    Patient Model and his relationship with User

---
+++ SmartTeethCare.Repository/Data/Configurations/PatientConfigurations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartTeethCare.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTeethCare.Repository.Data.Configurations
+{
+    public class PatientConfiguration : IEntityTypeConfiguration<Patient>
+    {
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+
+
+            builder.HasOne(p => p.User)
+                .WithOne(u => u.Patient)
+                .HasForeignKey<Patient>(p => p.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
