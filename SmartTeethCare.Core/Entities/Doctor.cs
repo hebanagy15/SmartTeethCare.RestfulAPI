8cfc90591aa37acdf88054967436aac280b2d3de
Heba Nagy <hebanagy3333@gmail.com>
2025-10-23 20:07:56 +0300

    Doctor Model and relationship with user

---
+++ SmartTeethCare.Core/Entities/Doctor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTeethCare.Core.Entities
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public int Salary { get; set; }
+        public int WorkingHours { get; set; }
+        public DateTime HiringDate { get; set; }
+
+        // Relation with ApplicationUser
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ SmartTeethCare.Repository/Data/Configurations/DoctorConfigurations.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartTeethCare.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTeethCare.Repository.Data.Configurations
+{
+    public class DoctorConfiguration : IEntityTypeConfiguration<Doctor>
+    {
+        public void Configure(EntityTypeBuilder<Doctor> builder)
+        {
+
+
+            // User - Doctor relation
+
+            builder.HasOne(d => d.User)
+                .WithOne(u => u.Doctor)
+                .HasForeignKey<Doctor>(d => d.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
